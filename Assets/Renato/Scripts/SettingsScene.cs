0956a918b5c300e29361ef8ee022ed4e993f6e8b
Renato <renatoburgos49@gmail.com>
2023-09-27 18:50:03 -0300

    Settings scene

--- Assets/Renato/Scripts/SettingsScene.cs
+++ Assets/Renato/Scripts/SettingsScene.cs
@@ -1,9 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class SettingsScene : MonoBehaviour {
+    public Slider[] sliders;
     void Start() {
         
     }
@@ -17,6 +20,17 @@ public class SettingsScene : MonoBehaviour {
     }
 
     public void SaveChanges() {
+        float[] values = new float[sliders.Length];
+        for (int i = 0; i < sliders.Length; i++) {
+            values[i] = sliders[i].value;
+        }
+        Settings settings = new Settings(values);
         SceneManager.LoadScene("FirstScene");
+    }
+
+    public void ChangedValue(GameObject parent) {
+        TMP_Text text = parent.transform.Find("Number").GetComponent<TMP_Text>();
+        float number = parent.transform.Find("Slider").GetComponent<Slider>().value;
+        text.SetText(number.ToString());
     }
 }
